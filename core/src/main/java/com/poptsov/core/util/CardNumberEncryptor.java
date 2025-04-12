cf74d87e9400a414ad9b90067a8d694d273bbde8
Poptsov Roman <poptsov.roman@mail.ru>
2025-04-13 03:28:03 +0500

    fix cardNumberEncryptor

--- core/src/main/java/com/poptsov/core/util/CardNumberEncryptor.java
+++ core/src/main/java/com/poptsov/core/util/CardNumberEncryptor.java
@@ -31,7 +31,34 @@ public class CardNumberEncryptor {
     }
 
 
-    public String mask(String cardNumber) {
-        return "**** **** **** " + cardNumber.substring(15);
+    public String mask(String encryptedCardNumber) {
+        if (encryptedCardNumber == null || encryptedCardNumber.isEmpty()) {
+            return "**** **** **** ****";
+        }
+
+        try {
+            String decrypted = decrypt(encryptedCardNumber);
+
+            String cleanNumber = decrypted.replaceAll("\\s+", "");
+
+            String lastFour = cleanNumber.substring(cleanNumber.length() - 4);
+
+            return "**** **** **** " + lastFour;
+        } catch (Exception e) {
+            return "**** **** **** ****";
+        }
+    }
+
+    public String decrypt(String encryptedData) {
+        try {
+            SecretKeySpec key = new SecretKeySpec(SECRET_KEY.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] decoded = Base64.getDecoder().decode(encryptedData);
+            byte[] decrypted = cipher.doFinal(decoded);
+            return new String(decrypted);
+        } catch (Exception e) {
+            throw new RuntimeException("Decryption failed", e);
+        }
     }
 }
