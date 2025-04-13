ffadfc7bd1d840b69ddc0a764e76b93ec2a25f23
Poptsov Roman <poptsov.roman@mail.ru>
2025-04-13 06:10:02 +0500

    add logback to gitignore

--- core/src/main/java/com/poptsov/core/repository/TransactionLimitRepository.java
+++ core/src/main/java/com/poptsov/core/repository/TransactionLimitRepository.java
@@ -2,9 +2,14 @@ package com.poptsov.core.repository;
 
 import com.poptsov.core.model.LimitType;
 import com.poptsov.core.model.TransactionLimit;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface TransactionLimitRepository extends JpaRepository<TransactionLimit, Long> {
+    @EntityGraph(attributePaths = {"card"})
     Optional<TransactionLimit> findByCardIdAndLimitType(Long cardId, LimitType limitType);
 }
